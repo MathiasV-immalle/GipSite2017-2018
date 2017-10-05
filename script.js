1006d027514cedce4bc4403b85c0f00f1bbd7541
DESKTOP-CP2B0BD\Mathias Vansant <mathias.vansant@immalle.be>
2017-10-05 15:21:50 +0200

    added pictures fix and use of placeholder

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var howOften = 4; // 4 seconds
 var current = Math.floor(Math.random() * 7);
 var ns6 = document.getElementById&&!document.all;
 var items = new Array()
-	items[0]="<img alt='' src='' width='96%' border='0'/>";
-	items[1]="<img alt='' src='' width='96%' border='0' />";
-	items[2]="<img alt='' src='' width='96%' border='0' />";
-	items[3]="<img alt='' src='' width='96%' border='0' />";
-	items[4]="<img alt='' src='' width='96%' border='0' />";
-	items[5]="<img class='verticalfoto' alt='' src='' width='55%' border='0' />";
-	items[6]="<img class='verticalfoto' alt='' src='' width='70%' border='0' />";
+	items[0]="<img alt='afb' src='afb/DesktopTesten.jpg' width='93%' border='0' />";
+	items[1]="<img alt='afb' src='afb/DesktopTesten2.jpg' width='93%' border='0' />";
+	items[2]="<img alt='abf' src='afb/OpenDesktop.jpg' width='93%' border='0' />";
+	items[3]="<img alt='afb' src='afb/OpeniPhone.jpg' width='93%' border='0' />";
+	items[4]="<img alt='afb' src='afb/OpenSamsung.jpg' width='93%' border='0' />";
+	items[5]="<img alt='afb' src='afb/Werkbasis2.jpg' width='93%' border='0' />";
+	items[6]="<img alt='afb' src='afb/Werkplatform.jpg' width='93%' border='0' />";
 function rotater() {
 	document.getElementById("placeholder").innerHTML = items[current];
 	current = (current==items.length-1) ? 0 : current + 1;
